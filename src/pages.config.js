3448ccd48a55284841a5d6ea29b63acdf93839a0
billy.nelms <billy.nelms@fbca.org>
2025-10-29 03:50:52 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,55 +1,25 @@
-import Loading from './pages/Loading';
-import Dashboard from './pages/Dashboard';
-import Marketing from './pages/Marketing';
-import FoodService from './pages/FoodService';
-import Settings from './pages/Settings';
-import IntegrationTest from './pages/IntegrationTest';
-import Documents from './pages/Documents';
-import Search from './pages/Search';
-import StaffDirectory from './pages/StaffDirectory';
-import MicrosoftLogin from './pages/MicrosoftLogin';
-import Onboarding from './pages/Onboarding';
-import MyTasks from './pages/MyTasks';
-import AIHelper from './pages/AIHelper';
-import MyApprovals from './pages/MyApprovals';
-import TaskDetail from './pages/TaskDetail';
-import BrandAssets from './pages/BrandAssets';
-import Ticketing from './pages/Ticketing';
-import MyDepartment from './pages/MyDepartment';
-import InboxHelper from './pages/InboxHelper';
-import MyMeetings from './pages/MyMeetings';
-import PCODebug from './pages/PCODebug';
-import  from './pages/';
-import Layout from './Layout.jsx';
+export const pagesConfig = [
+  { path: '/Loading', file: 'Loading' },
+  { path: '/Dashboard', file: 'Dashboard' },
+  { path: '/Marketing', file: 'Marketing' },
+  { path: '/FoodService', file: 'FoodService' },
+  { path: '/Settings', file: 'Settings' },
+  { path: '/IntegrationTest', file: 'IntegrationTest' },
+  { path: '/Documents', file: 'Documents' },
+  { path: '/Search', file: 'Search' },
+  { path: '/StaffDirectory', file: 'StaffDirectory' },
+  { path: '/MicrosoftLogin', file: 'MicrosoftLogin' },
+  { path: '/Onboarding', file: 'Onboarding' },
+  { path: '/MyTasks', file: 'MyTasks' },
+  { path: '/AIHelper', file: 'AIHelper' },
+  { path: '/MyApprovals', file: 'MyApprovals' },
+  { path: '/TaskDetail', file: 'TaskDetail' },
+  { path: '/BrandAssets', file: 'BrandAssets' },
+  { path: '/Ticketing', file: 'Ticketing' },
+  { path: '/MyDepartment', file: 'MyDepartment' },
+  { path: '/InboxHelper', file: 'InboxHelper' },
+  { path: '/MyMeetings', file: 'MyMeetings' },
+  { path: '/PCODebug', file: 'PCODebug' }
+];
 
-
-export const PAGES = {
-    "Loading": Loading,
-    "Dashboard": Dashboard,
-    "Marketing": Marketing,
-    "FoodService": FoodService,
-    "Settings": Settings,
-    "IntegrationTest": IntegrationTest,
-    "Documents": Documents,
-    "Search": Search,
-    "StaffDirectory": StaffDirectory,
-    "MicrosoftLogin": MicrosoftLogin,
-    "Onboarding": Onboarding,
-    "MyTasks": MyTasks,
-    "AIHelper": AIHelper,
-    "MyApprovals": MyApprovals,
-    "TaskDetail": TaskDetail,
-    "BrandAssets": BrandAssets,
-    "Ticketing": Ticketing,
-    "MyDepartment": MyDepartment,
-    "InboxHelper": InboxHelper,
-    "MyMeetings": MyMeetings,
-    "PCODebug": PCODebug,
-    "": ,
-}
-
-export const pagesConfig = {
-    mainPage: "Loading",
-    Pages: PAGES,
-    Layout: Layout,
-};
+export default pagesConfig;
