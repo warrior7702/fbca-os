30c7fafb468a3ce6d0d52aa77848d49e431b13b1
billy.nelms <billy.nelms@fbca.org>
2025-11-06 15:17:50 +0000

    File changes

--- src/pages/MyTasks.jsx
+++ src/pages/MyTasks.jsx
@@ -405,7 +405,7 @@ export default function MyTasks() {
             ) : (
               <ScheduleCalendar 
                 events={myScheduleEvents} 
-                weekCount={1}
+                weekCount={3}
                 onEventClick={handleScheduleEventClick}
               />
             )}
