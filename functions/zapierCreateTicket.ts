57ccb06767fcb62799d507ace691b7409f585d1c
billy.nelms <billy.nelms@fbca.org>
2025-11-19 23:10:57 +0000

    File changes

--- functions/zapierCreateTicket.ts
+++ functions/zapierCreateTicket.ts
@@ -1,8 +1,8 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
+import { createClient } from 'npm:@base44/sdk@0.8.4';
 
 Deno.serve(async (req) => {
   try {
-    const base44 = createClientFromRequest(req);
+    const base44 = createClient().asServiceRole;
 
     // Incoming JSON payload from Zapier
     const {
