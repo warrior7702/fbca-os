9f9c4df310059a3e24247eb7745d8563ab504587
billy.nelms <billy.nelms@fbca.org>
2025-12-02 04:27:31 +0000

    Tasks clickable, open modal

--- src/components/tasks/DeptTaskDetailModal.jsx
+++ src/components/tasks/DeptTaskDetailModal.jsx
@@ -151,13 +151,13 @@ export default function DeptTaskDetailModal({
             {isEditing ? (
               <Input
                 type="date"
-                value={editedTask.dueDate ? (editedTask.dueDate.includes('T') ? editedTask.dueDate.split('T')[0] : editedTask.dueDate) : ''}
+                value={editedTask.dueDate?.split('T')[0] || ''}
                 onChange={(e) => setEditedTask({ ...editedTask, dueDate: e.target.value })}
               />
             ) : (
               <div className="flex items-center gap-2">
                 <Calendar className="w-4 h-4 text-teal-600" />
-                <span className="font-medium">{task.dueDate ? format(new Date(task.dueDate), 'EEEE, MMMM d, yyyy') : 'No due date'}</span>
+                <span className="font-medium">{format(new Date(task.dueDate), 'EEEE, MMMM d, yyyy')}</span>
               </div>
             )}
           </div>
