47721d8db02b25ba099f0fd6ddda545587a78333
billy.nelms <billy.nelms@fbca.org>
2025-12-03 03:32:44 +0000

    File changes

--- src/pages/MyDepartment.jsx
+++ src/pages/MyDepartment.jsx
@@ -36,7 +36,8 @@ import {
   UserPlus,
   CalendarClock,
   Send,
-  LayoutGrid
+  LayoutGrid,
+  Folder
 } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
