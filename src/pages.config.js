a5f9a670ab6c0cd3cfc52d796613db44b8b3b0b6
billy.nelms <billy.nelms@fbca.org>
2025-12-23 05:35:37 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,133 +1,143 @@
-import Loading from './pages/Loading';
-import Dashboard from './pages/Dashboard';
-import Marketing from './pages/Marketing';
-import FoodService from './pages/FoodService';
-import Settings from './pages/Settings';
-import IntegrationTest from './pages/IntegrationTest';
-import Documents from './pages/Documents';
-import Search from './pages/Search';
-import StaffDirectory from './pages/StaffDirectory';
-import MicrosoftLogin from './pages/MicrosoftLogin';
-import Onboarding from './pages/Onboarding';
-import MyTasks from './pages/MyTasks';
 import AIHelper from './pages/AIHelper';
-import MyApprovals from './pages/MyApprovals';
-import TaskDetail from './pages/TaskDetail';
-import BrandAssets from './pages/BrandAssets';
-import Ticketing from './pages/Ticketing';
-import MyDepartment from './pages/MyDepartment';
-import InboxHelper from './pages/InboxHelper';
-import MyMeetings from './pages/MyMeetings';
-import PCODebug from './pages/PCODebug';
-import index from './pages/index';
-import ImportCardholders from './pages/ImportCardholders';
-import TestCardholders from './pages/TestCardholders';
-import AdminSetup from './pages/AdminSetup';
-import Calendar from './pages/Calendar';
-import SupportTickets from './pages/SupportTickets';
-import CreateTicket from './pages/CreateTicket';
-import PCOAPITester from './pages/PCOAPITester';
-import PhantomUserHunter from './pages/PhantomUserHunter';
-import WorkflowHub from './pages/WorkflowHub';
-import TestMysterySync from './pages/TestMysterySync';
-import CommunicationsRequestForm from './pages/CommunicationsRequestForm';
-import WorkflowDetail from './pages/WorkflowDetail';
-import ProjectReview from './pages/ProjectReview';
-import DiagnoseMysteryResource from './pages/DiagnoseMysteryResource';
-import CronStatus from './pages/CronStatus';
-import EmailTemplateEditor from './pages/EmailTemplateEditor';
-import CampaignRunning from './pages/CampaignRunning';
-import TicketDetail from './pages/TicketDetail';
-import TicketAnalytics from './pages/TicketAnalytics';
-import MediaPlayer from './pages/MediaPlayer';
 import Achievements from './pages/Achievements';
-import DepartmentTest from './pages/DepartmentTest';
-import TicketRoleVerification from './pages/TicketRoleVerification';
-import Buildings from './pages/Buildings';
-import AkitaFetch from './pages/AkitaFetch';
+import AdminSetup from './pages/AdminSetup';
 import AkitaBoxExplorer from './pages/AkitaBoxExplorer';
-import MeetingNotes from './pages/MeetingNotes';
-import TestTranscription from './pages/TestTranscription';
-import VoiceProfiles from './pages/VoiceProfiles';
-import SharePoint from './pages/SharePoint';
-import Notifications from './pages/Notifications';
-import RoleManagement from './pages/RoleManagement';
+import AkitaFetch from './pages/AkitaFetch';
+import AkitaSyncAdmin from './pages/AkitaSyncAdmin';
+import Approvals from './pages/Approvals';
 import AssignmentRules from './pages/AssignmentRules';
-import Knowledgebase from './pages/Knowledgebase';
-import TicketReporting from './pages/TicketReporting';
+import BackfillEventCodes from './pages/BackfillEventCodes';
+import BrandAssets from './pages/BrandAssets';
+import Buildings from './pages/Buildings';
+import Calendar from './pages/Calendar';
+import CampaignRunning from './pages/CampaignRunning';
+import CommunicationsRequestForm from './pages/CommunicationsRequestForm';
+import CreateTicket from './pages/CreateTicket';
+import CronStatus from './pages/CronStatus';
+import Dashboard from './pages/Dashboard';
+import DepartmentTest from './pages/DepartmentTest';
+import DiagnoseMysteryResource from './pages/DiagnoseMysteryResource';
+import Documents from './pages/Documents';
+import EmailTemplateEditor from './pages/EmailTemplateEditor';
+import FBCANexts from './pages/FBCANexts';
+import FloorPlanManager from './pages/FloorPlanManager';
+import FoodService from './pages/FoodService';
+import Home from './pages/Home';
+import ImportCardholders from './pages/ImportCardholders';
 import ImportClickUpTickets from './pages/ImportClickUpTickets';
 import ImportTickets from './pages/ImportTickets';
+import InboxHelper from './pages/InboxHelper';
+import IntegrationTest from './pages/IntegrationTest';
+import Knowledgebase from './pages/Knowledgebase';
+import Loading from './pages/Loading';
+import Marketing from './pages/Marketing';
+import Me from './pages/Me';
+import MediaPlayer from './pages/MediaPlayer';
+import MeetingNotes from './pages/MeetingNotes';
+import MicrosoftLogin from './pages/MicrosoftLogin';
+import MyApprovals from './pages/MyApprovals';
+import MyDepartment from './pages/MyDepartment';
+import MyMeetings from './pages/MyMeetings';
+import MyTasks from './pages/MyTasks';
 import NormalizeUserNames from './pages/NormalizeUserNames';
-import BackfillEventCodes from './pages/BackfillEventCodes';
-import FloorPlanManager from './pages/FloorPlanManager';
-import AkitaSyncAdmin from './pages/AkitaSyncAdmin';
+import Notifications from './pages/Notifications';
+import Onboarding from './pages/Onboarding';
+import PCOAPITester from './pages/PCOAPITester';
+import PCODebug from './pages/PCODebug';
+import PhantomUserHunter from './pages/PhantomUserHunter';
+import ProjectReview from './pages/ProjectReview';
+import RoleManagement from './pages/RoleManagement';
+import Search from './pages/Search';
+import Settings from './pages/Settings';
+import SharePoint from './pages/SharePoint';
+import StaffDirectory from './pages/StaffDirectory';
+import SupportTickets from './pages/SupportTickets';
+import TaskDetail from './pages/TaskDetail';
+import Tasks from './pages/Tasks';
+import TestCardholders from './pages/TestCardholders';
+import TestMysterySync from './pages/TestMysterySync';
+import TestTranscription from './pages/TestTranscription';
+import TicketAnalytics from './pages/TicketAnalytics';
+import TicketDetail from './pages/TicketDetail';
+import TicketReporting from './pages/TicketReporting';
+import TicketRoleVerification from './pages/TicketRoleVerification';
+import Ticketing from './pages/Ticketing';
+import VoiceProfiles from './pages/VoiceProfiles';
+import WorkflowDetail from './pages/WorkflowDetail';
+import WorkflowHub from './pages/WorkflowHub';
+import index from './pages/index';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Loading": Loading,
-    "Dashboard": Dashboard,
-    "Marketing": Marketing,
-    "FoodService": FoodService,
-    "Settings": Settings,
-    "IntegrationTest": IntegrationTest,
-    "Documents": Documents,
-    "Search": Search,
-    "StaffDirectory": StaffDirectory,
-    "MicrosoftLogin": MicrosoftLogin,
-    "Onboarding": Onboarding,
-    "MyTasks": MyTasks,
     "AIHelper": AIHelper,
-    "MyApprovals": MyApprovals,
-    "TaskDetail": TaskDetail,
-    "BrandAssets": BrandAssets,
-    "Ticketing": Ticketing,
-    "MyDepartment": MyDepartment,
-    "InboxHelper": InboxHelper,
-    "MyMeetings": MyMeetings,
-    "PCODebug": PCODebug,
-    "index": index,
-    "ImportCardholders": ImportCardholders,
-    "TestCardholders": TestCardholders,
-    "AdminSetup": AdminSetup,
-    "Calendar": Calendar,
-    "SupportTickets": SupportTickets,
-    "CreateTicket": CreateTicket,
-    "PCOAPITester": PCOAPITester,
-    "PhantomUserHunter": PhantomUserHunter,
-    "WorkflowHub": WorkflowHub,
-    "TestMysterySync": TestMysterySync,
-    "CommunicationsRequestForm": CommunicationsRequestForm,
-    "WorkflowDetail": WorkflowDetail,
-    "ProjectReview": ProjectReview,
-    "DiagnoseMysteryResource": DiagnoseMysteryResource,
-    "CronStatus": CronStatus,
-    "EmailTemplateEditor": EmailTemplateEditor,
-    "CampaignRunning": CampaignRunning,
-    "TicketDetail": TicketDetail,
-    "TicketAnalytics": TicketAnalytics,
-    "MediaPlayer": MediaPlayer,
     "Achievements": Achievements,
-    "DepartmentTest": DepartmentTest,
-    "TicketRoleVerification": TicketRoleVerification,
-    "Buildings": Buildings,
-    "AkitaFetch": AkitaFetch,
+    "AdminSetup": AdminSetup,
     "AkitaBoxExplorer": AkitaBoxExplorer,
-    "MeetingNotes": MeetingNotes,
-    "TestTranscription": TestTranscription,
-    "VoiceProfiles": VoiceProfiles,
-    "SharePoint": SharePoint,
-    "Notifications": Notifications,
-    "RoleManagement": RoleManagement,
+    "AkitaFetch": AkitaFetch,
+    "AkitaSyncAdmin": AkitaSyncAdmin,
+    "Approvals": Approvals,
     "AssignmentRules": AssignmentRules,
-    "Knowledgebase": Knowledgebase,
-    "TicketReporting": TicketReporting,
+    "BackfillEventCodes": BackfillEventCodes,
+    "BrandAssets": BrandAssets,
+    "Buildings": Buildings,
+    "Calendar": Calendar,
+    "CampaignRunning": CampaignRunning,
+    "CommunicationsRequestForm": CommunicationsRequestForm,
+    "CreateTicket": CreateTicket,
+    "CronStatus": CronStatus,
+    "Dashboard": Dashboard,
+    "DepartmentTest": DepartmentTest,
+    "DiagnoseMysteryResource": DiagnoseMysteryResource,
+    "Documents": Documents,
+    "EmailTemplateEditor": EmailTemplateEditor,
+    "FBCANexts": FBCANexts,
+    "FloorPlanManager": FloorPlanManager,
+    "FoodService": FoodService,
+    "Home": Home,
+    "ImportCardholders": ImportCardholders,
     "ImportClickUpTickets": ImportClickUpTickets,
     "ImportTickets": ImportTickets,
+    "InboxHelper": InboxHelper,
+    "IntegrationTest": IntegrationTest,
+    "Knowledgebase": Knowledgebase,
+    "Loading": Loading,
+    "Marketing": Marketing,
+    "Me": Me,
+    "MediaPlayer": MediaPlayer,
+    "MeetingNotes": MeetingNotes,
+    "MicrosoftLogin": MicrosoftLogin,
+    "MyApprovals": MyApprovals,
+    "MyDepartment": MyDepartment,
+    "MyMeetings": MyMeetings,
+    "MyTasks": MyTasks,
     "NormalizeUserNames": NormalizeUserNames,
-    "BackfillEventCodes": BackfillEventCodes,
-    "FloorPlanManager": FloorPlanManager,
-    "AkitaSyncAdmin": AkitaSyncAdmin,
+    "Notifications": Notifications,
+    "Onboarding": Onboarding,
+    "PCOAPITester": PCOAPITester,
+    "PCODebug": PCODebug,
+    "PhantomUserHunter": PhantomUserHunter,
+    "ProjectReview": ProjectReview,
+    "RoleManagement": RoleManagement,
+    "Search": Search,
+    "Settings": Settings,
+    "SharePoint": SharePoint,
+    "StaffDirectory": StaffDirectory,
+    "SupportTickets": SupportTickets,
+    "TaskDetail": TaskDetail,
+    "Tasks": Tasks,
+    "TestCardholders": TestCardholders,
+    "TestMysterySync": TestMysterySync,
+    "TestTranscription": TestTranscription,
+    "TicketAnalytics": TicketAnalytics,
+    "TicketDetail": TicketDetail,
+    "TicketReporting": TicketReporting,
+    "TicketRoleVerification": TicketRoleVerification,
+    "Ticketing": Ticketing,
+    "VoiceProfiles": VoiceProfiles,
+    "WorkflowDetail": WorkflowDetail,
+    "WorkflowHub": WorkflowHub,
+    "index": index,
 }
 
 export const pagesConfig = {
