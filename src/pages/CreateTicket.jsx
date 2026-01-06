044f0ce920ce452583718b6e862bdd1af9ba5d1a
billy.nelms <billy.nelms@fbca.org>
2026-01-06 19:44:58 +0000

    File changes

--- src/pages/CreateTicket.jsx
+++ src/pages/CreateTicket.jsx
@@ -15,6 +15,7 @@ import {
 import { format } from "date-fns";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import {
